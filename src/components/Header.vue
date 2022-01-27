3dfe734f5b04650e9a2548f825974299a087f9a8
Mehedi Hasan <mehedifci907@gmail.com>
2022-01-27 17:47:30 +0600

    Update default title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,7 @@ export default{
     props:{
         title:{
             type:String,
-            default:"Task Tracker"
+            default:"Task Tracker App"
         },
         showAddTask:{
             type:Boolean
